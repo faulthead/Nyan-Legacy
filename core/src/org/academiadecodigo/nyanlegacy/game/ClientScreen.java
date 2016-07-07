8e2b8a388e0ec5bfe995caf86d42ce1bb610e6f1
João Claro <joao.claro.ck@gmail.com>
2016-07-07 21:21:37 +0100

    Finished Implementing the keyboard handling
    Ended up having to remove the Player and PlayerLogic Classes due to the fact that libgdx does not allow me to have a separate ApplicationAdapter and InputProcessor in a easy way.

---
+++ core/src/org/academiadecodigo/nyanlegacy/game/ClientScreen.java
@@ -0,0 +1,135 @@
+package org.academiadecodigo.nyanlegacy.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+import static com.badlogic.gdx.Input.Keys.*;
+
+public class ClientScreen extends ApplicationAdapter implements InputProcessor{
+
+    private SpriteBatch batch;
+    private Texture img;
+
+    private DatagramSocket playerSocket;
+
+    private boolean movingLeft = false;
+    private boolean movingRight = false;
+    private boolean movingUp = false;
+    private boolean movingDown = false;
+
+    @Override
+    public void create() {
+
+        try {
+            playerSocket = new DatagramSocket();
+        } catch (SocketException e) {
+            System.out.println(e.getMessage());
+        }
+
+        batch = new SpriteBatch();
+        img = new Texture("core/assets/background.png");
+
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void render() {
+
+        if (movingLeft) {
+
+        }
+        if (movingRight) {
+
+        }
+        if (movingUp) {
+
+        }
+        if (movingDown) {
+
+        }
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == LEFT) {
+            movingLeft = true;
+        }
+        if (keycode == RIGHT) {
+            movingRight = true;
+        }
+        if (keycode == UP) {
+            movingUp = true;
+        }
+        if (keycode == DOWN) {
+            movingDown = true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (keycode == LEFT) {
+            movingLeft = false;
+        }
+        if (keycode == RIGHT) {
+            movingRight = false;
+        }
+        if (keycode == UP) {
+            movingUp = false;
+        }
+        if (keycode == DOWN) {
+            movingDown = false;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
