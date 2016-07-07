bf10f6096fdc55d08345813738a3359a155bea98
João Claro <joao.claro.ck@gmail.com>
2016-07-08 00:43:24 +0100

    Finished the controllers with the new Client classes, moving on to networking

--- core/src/org/academiadecodigo/nyanlegacy/game/GameManager.java
+++ core/src/org/academiadecodigo/nyanlegacy/game/GameManager.java
@@ -1,13 +1,16 @@
 package org.academiadecodigo.nyanlegacy.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by Cadavre Exquis on 07-07-2016.
  */
-public class GameManager extends Game {
+public class GameManager extends Game implements InputProcessor{
 
     //Game norms and constants that are used by various classes
 
@@ -18,6 +21,13 @@ public class GameManager extends Game {
     public static final short NYAN_BIT = 2;
     public static final short PINK_NYAN_BIT = 4;
     public static final short CLOUD_BIT = 8;
+
+
+    //Input processor stuff.
+    private boolean movingLeft = false;
+    private boolean movingRight = false;
+    private boolean movingUp = false;
+    private boolean movingDown = false;
 
     public SpriteBatch spriteBatch;
 
@@ -31,12 +41,26 @@ public class GameManager extends Game {
         //manager.load("path", Music.class);
         //manager.finishLoading();
 
+        Gdx.input.setInputProcessor(this);
+
         setScreen(new ClientScreen(this, manager));
     }
 
     @Override
     public void render() {
         super.render();
+        if (movingLeft == true) {
+            System.out.println("left");
+        }
+        if (movingRight == true) {
+            System.out.println("right");
+        }
+        if (movingDown == true) {
+            System.out.println("down");
+        }
+        if (movingUp == true) {
+            System.out.println("up");
+        }
     }
 
     @Override
@@ -44,5 +68,74 @@ public class GameManager extends Game {
         super.dispose();
         manager.dispose();
         spriteBatch.dispose();
+    }
+
+
+    @Override
+    public boolean keyDown(int keycode) {
+
+        if (keycode == Input.Keys.LEFT) {
+            movingLeft = true;
+        }
+        if (keycode == Input.Keys.RIGHT) {
+            movingRight = true;
+        }
+        if (keycode == Input.Keys.UP) {
+            movingUp = true;
+        }
+        if (keycode == Input.Keys.DOWN) {
+            movingDown = true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+
+        if (keycode == Input.Keys.LEFT) {
+            movingLeft = false;
+        }
+        if (keycode == Input.Keys.RIGHT) {
+            movingRight = false;
+        }
+        if (keycode == Input.Keys.UP) {
+            movingUp = false;
+        }
+        if (keycode == Input.Keys.DOWN) {
+            movingDown = false;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
