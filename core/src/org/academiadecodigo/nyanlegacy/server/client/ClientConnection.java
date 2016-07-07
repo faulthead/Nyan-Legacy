0753e7d5a3f15d1a8e1dc4c5badf1aba9b164cbf
Igor Estevao <IgorFIE@hotmail.com>
2016-07-07 21:35:09 +0100

    Server and clientConnection

--- core/src/org/academiadecodigo/nyanlegacy/server/client/ClientConnection.java
+++ core/src/org/academiadecodigo/nyanlegacy/server/client/ClientConnection.java
@@ -3,21 +3,109 @@ package org.academiadecodigo.nyanlegacy.server.client;
 import org.academiadecodigo.nyanlegacy.server.Server;
 import org.academiadecodigo.nyanlegacy.server.ServerLogic;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  * Created by joaoc on 07/07/2016.
  */
 public class ClientConnection implements Runnable {
 
+    private Server server;
+    private DatagramSocket socket;
+    private InetAddress address;
+    private int port;
+
+    private byte[] sendData;
+    private byte[] receiveData;
+
     private Position position;
+    private Position pixelPosition;
     private boolean isDead;
 
-    public ClientConnection(Server server, ServerLogic serverLogic) {
+    public ClientConnection(Server server, DatagramSocket socket, InetAddress address, int port) {
+        this.server = server;
+        this.socket = socket;
+        this.address = address;
+        this.port = port;
+        sendData = new byte[1024];
+        receiveData = new byte[1024];
     }
 
     @Override
     public void run() {
 
+        while (true) {
+            try {
+                if (server.isGameStarted()) {
+
+                    if(!isDead) {
+
+                        DatagramPacket receiveMessage = new DatagramPacket(receiveData, receiveData.length);
+                        socket.receive(receiveMessage);
+
+                        String message = new String(receiveData, 0, receiveMessage.getLength());
+
+                        move(message);
+
+                        server.sendToAll(toString());
+                    }
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
+
+    /**
+     * Movement logic
+     *
+     * @param move the respective movement to make
+     */
+    private void move(String move){
+        if(move.equals("up") || position.getRow() > 0){
+            isDead = ServerLogic.getInstance().collision(position.getCol(),position.getRow());
+        }
+        if(move.equals("down") || position.getRow() < ServerLogic.getInstance().CELLSIZE - 1){
+            isDead = ServerLogic.getInstance().collision(position.getCol(),position.getRow());
+        }
+        if(move.equals("left") || position.getCol() > 0){
+            isDead = ServerLogic.getInstance().collision(position.getCol(),position.getRow());
+        }
+        if(move.equals("right") || position.getCol() < ServerLogic.getInstance().CELLSIZE - 1){
+            isDead = ServerLogic.getInstance().collision(position.getCol(),position.getRow());
+        }
+    }
+
+    /**
+     * generates a String with the respective attributes of the client
+     * @return a String
+     */
+    public String toString() {
+        return "" + address + ":" + pixelPosition.getCol() + ":" + pixelPosition.getRow() + ":" + isDead + "";
+    }
+
+    /**
+     * Send a mensage to the client connected
+     *
+     * @param message to send
+     */
+    public void send(String message) {
+        sendData = message.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, port);
+        try {
+            socket.send(sendPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //GETTERS AND SETTERS
 
     public Position getPosition() {
         return position;
@@ -27,11 +115,15 @@ public class ClientConnection implements Runnable {
         this.position = position;
     }
 
-    public boolean isDead() {
-        return isDead;
-    }
-
     public void setDead(boolean dead) {
         isDead = dead;
+    }
+
+    public Position getPixelPosition() {
+        return pixelPosition;
+    }
+
+    public void setPixelPosition(Position pixelPosition) {
+        this.pixelPosition = pixelPosition;
     }
 }
