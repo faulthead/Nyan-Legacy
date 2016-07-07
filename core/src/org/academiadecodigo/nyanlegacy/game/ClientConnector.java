97a5b45b53aa4d5fc95e10500dc99441992d1e9a
João Claro <joao.claro.ck@gmail.com>
2016-07-08 00:26:39 +0100

    Made a connector test

---
+++ core/src/org/academiadecodigo/nyanlegacy/game/ClientConnector.java
@@ -0,0 +1,30 @@
+package org.academiadecodigo.nyanlegacy.game;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * Created by joaoc on 07/07/2016.
+ */
+public class ClientConnector {
+    public static void main(String args[]) throws Exception {
+        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("192.168.1.27");
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        String sentence = inFromUser.readLine();
+        sendData = sentence.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 8080);
+        clientSocket.send(sendPacket);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        String modifiedSentence = new String(receivePacket.getData());
+        System.out.println("FROM SERVER:" + modifiedSentence);
+        clientSocket.close();
+    }
+}
